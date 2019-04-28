6398a1e7148b270d2283b6cbffaf7ff1cede25c2
Win <synwe@yahoo.com>
2019-04-28 01:48:12 +0000

    Start tests

---
+++ test/widget_test.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:folder_picker/folder_picker.dart';
+
+void main() async {
+  final directory = await Directory.systemTemp.createTemp();
+/*
+  testWidgets('Pick', (WidgetTester tester) async {
+    // Build the Widget
+    await tester.pumpWidget(FolderPicker(
+      rootDirectory: directory,
+      action: (context, folder) {
+        print("PICKED $folder");
+      },
+    ));
+    await tester.tap(find.byType(ListTile).first);
+  });*/
+}
