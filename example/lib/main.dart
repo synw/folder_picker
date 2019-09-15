695975f511f33abd70e1b8f8c90dc4c148039b53
synw <ansterj@gmail.com>
2019-09-16 00:09:05 +0200

    Fix read storage permission in example #2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission/permission.dart';
 import 'package:folder_picker/folder_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -7,11 +8,38 @@ class _FolderPickerPageState extends State<FolderPickerPage> {
   Directory externalDirectory;
   Directory pickedDirectory;
 
+  Future<void> getPermissions() async {
+    final permissions =
+        await Permission.getPermissionsStatus([PermissionName.Storage]);
+    var request = true;
+    switch (permissions[0].permissionStatus) {
+      case PermissionStatus.allow:
+        request = false;
+        break;
+      case PermissionStatus.always:
+        request = false;
+        break;
+      default:
+    }
+    if (request) {
+      await Permission.requestPermissions([PermissionName.Storage]);
+    }
+  }
+
+  Future<void> getStorage() async {
+    final directory = await getExternalStorageDirectory();
+    setState(() => externalDirectory = directory);
+  }
+
+  Future<void> init() async {
+    await getPermissions();
+    await getStorage();
+  }
+
   @override
   void initState() {
-    getExternalStorageDirectory()
-        .then((directory) => setState(() => externalDirectory = directory));
     super.initState();
+    init();
   }
 
   @override
@@ -26,16 +54,16 @@ class _FolderPickerPageState extends State<FolderPickerPage> {
                           child:
                               const Text("Pick a folder", textScaleFactor: 1.3),
                           onPressed: () => Navigator.of(context)
-                                  .push<FolderPicker>(MaterialPageRoute(
-                                      builder: (BuildContext context) {
-                                return FolderPicker(
-                                    rootDirectory: externalDirectory,
-                                    action: (BuildContext context,
-                                        Directory folder) async {
-                                      print("Picked directory $folder");
-                                      setState(() => pickedDirectory = folder);
-                                    });
-                              })),
+                              .push<FolderPicker>(MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                            return FolderPicker(
+                                rootDirectory: externalDirectory,
+                                action: (BuildContext context,
+                                    Directory folder) async {
+                                  print("Picked directory $folder");
+                                  setState(() => pickedDirectory = folder);
+                                });
+                          })),
                         ),
                         (pickedDirectory != null)
                             ? Padding(
