2b85bce64051d0f08a2bbb140bb72da5c2908b18
Marco Antonio Solis Lopez <moco@MarcBook.local>
2019-09-15 17:03:07 -0500

    Added create folder button, implemented AppBar for better visibility, changed selection button, renamed user variables to avoid confusion and implemented a better wey to prevent user from accessing forbidden folders

--- lib/src/pick.dart
+++ lib/src/pick.dart
@@ -8,28 +8,34 @@ import 'ls.dart';
 typedef Future<void> AfterPickedAction(BuildContext context, Directory folder);
 
 class _FolderPickerState extends State<FolderPicker> {
-  _FolderPickerState({@required this.action, @required this.rootDirectory}) {
-    _currentDirectory ??= rootDirectory;
+  _FolderPickerState({@required this.action, @required this.currentDirectory}) {
+    try {
+      ls(currentDirectory.parent);
+    } catch (e) {
+      currentDirectory = rootDirectory;
+    }
   }
 
+  final textControllerName = new TextEditingController(text: "New Folder");
   final AfterPickedAction action;
-  final Directory rootDirectory;
+  Directory rootDirectory;
 
-  Directory _currentDirectory;
+  Directory currentDirectory;
+  Widget bodyList;
 
   List<ListTile> _getDirs(BuildContext context) {
     var tiles = <ListTile>[];
-    List<Directory> dirs = ls(_currentDirectory);
+    List<Directory> dirs = ls(currentDirectory);
     dirs.forEach((dir) {
       tiles.add(ListTile(
           leading: IconButton(
               icon: const Icon(Icons.folder, color: Colors.yellow),
-              onPressed: () => setState(() => _currentDirectory = dir)),
+              onPressed: () => setState(() => currentDirectory = dir)),
           title: GestureDetector(
-              onTap: () => setState(() => _currentDirectory = dir),
+              onTap: () => setState(() => currentDirectory = dir),
               child: Text(basename(dir.path))),
           trailing: IconButton(
-              icon: const Icon(Icons.file_download),
+              icon: const Icon(Icons.check_circle),
               onPressed: () {
                 Navigator.of(context).pop();
                 action(context, dir);
@@ -40,22 +46,72 @@ class _FolderPickerState extends State<FolderPicker> {
 
   @override
   Widget build(BuildContext context) {
+    bodyList = ListView(children: _getDirs(context));
     return Scaffold(
-      body: ListView(
-        children: (_currentDirectory == rootDirectory)
-            ? _getDirs(context)
-            : [
-                GestureDetector(
-                  child: ListTile(
-                    leading: const Icon(Icons.arrow_upward),
-                    title: const Text("..", textScaleFactor: 1.5),
-                  ),
-                  onTap: () => setState(
-                      () => _currentDirectory = _currentDirectory.parent),
-                )
-              ]
-          ..addAll(_getDirs(context)),
+      appBar: AppBar(
+        leading: rootDirectory != currentDirectory
+            ? IconButton(
+                icon: Icon(Icons.arrow_upward),
+                onPressed: () {
+                  try {
+                    ls(currentDirectory.parent);
+                    setState(() => currentDirectory = currentDirectory.parent);
+                  } catch (e) {
+                    rootDirectory =
+                        currentDirectory; //Don't set state so the up
+                                          //arrow doesn't dissapear
+                  }
+                },
+              )
+            : null,
+        title: const Text("..", textScaleFactor: 1.5),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.create_new_folder),
+            onPressed: () {
+              return showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Create Folder'),
+                    content: SingleChildScrollView(
+                      child: ListBody(
+                        children: <Widget>[
+                          TextFormField(
+                            autofocus: false,
+                            controller: textControllerName,
+                          ),
+                        ],
+                      ),
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: const Text('Create'),
+                        onPressed: () {
+                          Directory(currentDirectory.path +
+                                  "/" +
+                                  textControllerName.text)
+                              .create(recursive: true)
+                              .then((Directory directory) {
+                            print(directory.path);
+                            setState(() {
+                              bodyList = ListView(children: _getDirs(context));
+                            });
+                          });
+
+                          Navigator.of(context).pop();
+                          textControllerName.text = "New Folder";
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
+      body: bodyList,
     );
   }
 }
@@ -63,15 +119,15 @@ class _FolderPickerState extends State<FolderPicker> {
 /// The folder picker page
 class FolderPicker extends StatefulWidget {
   /// Main constructor
-  FolderPicker({@required this.action, @required this.rootDirectory});
+  FolderPicker({@required this.action, @required this.currentDirectory});
 
   /// The action to execute after the file is picked
   final AfterPickedAction action;
 
   /// The directory to start browsing from
-  final Directory rootDirectory;
+  final Directory currentDirectory;
 
   @override
   _FolderPickerState createState() =>
-      _FolderPickerState(action: action, rootDirectory: rootDirectory);
+      _FolderPickerState(action: action, currentDirectory: currentDirectory);
 }
